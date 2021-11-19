db01b5f63888773f0f8b7e7febb6826885d124ca
leeandri <rl.tsirih@gmail.com>
2021-11-19 17:57:50 +0300

    09-create-service

---
+++ src/Service/MarkdownHelper.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace App\Service;
+
+
+class MarkdownHelper
+{
+    public function parse(string $source): string
+    {
+        return $cache->get('markdown_'.md5($source), function() use ($source,$markdownParser){
+            return $markdownParser->transformMarkdown($source,);
+        });
+    }
+}
